60da32c835b4ac6044e0ec98f42a753c7d926ce3
Anthony Minnaugh <anthony.minnaugh@scorpion.co>
2022-01-20 11:07:58 -0800

    Presentational Dumb Components

--- angular-fundamentals/src/app/passengar-dashboard/passengar-dashboard.module.ts
+++ angular-fundamentals/src/app/passengar-dashboard/passengar-dashboard.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { PassengarDashboardComponent } from "./containers/passenger-dashboard/passengar-dashboard.component";
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
 @NgModule({
     declarations: [
-        PassengarDashboardComponent
+        PassengarDashboardComponent,
+        PassengerCountComponent,
+        PassengerDetailComponent
     ],
     imports: [CommonModule],
     exports: [PassengarDashboardComponent]
