75d88feb3607f954ffd22ef361b32f9737d3d642
Anthony Minnaugh <anthony.minnaugh@scorpion.co>
2022-01-20 09:59:17 -0800

    Feature modules with @NgModule

--- angular-fundamentals/src/app/app.module.ts
+++ angular-fundamentals/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { CommonModule } from "@angular/common";
 
+import { PassengerDashboardModule } from './passengar-dashboard/passengar-dashboard.module';
+
 import { AppComponent } from "./app.component";
 
 @NgModule({
@@ -10,7 +12,8 @@ import { AppComponent } from "./app.component";
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    PassengerDashboardModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ angular-fundamentals/src/app/passengar-dashboard/passengar-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-passengar-dashboard',
+  templateUrl: './passengar-dashboard.component.html',
+  styleUrls: ['./passengar-dashboard.component.scss']
+})
+export class PassengarDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
