bc0703278ba4cd7d940eb1eb31c865e8f7aa65d5
Anthony Minnaugh <anthony.minnaugh@scorpion.co>
2022-01-19 16:03:38 -0800

    First component with @Component

--- angular-fundamentals/src/app/app.component.ts
+++ angular-fundamentals/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-fundamentals';
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular'
+  }
 }
